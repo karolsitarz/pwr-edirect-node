c0b827c35f32c75ff3cfa9edc827cb0105d3cda6
Karol Sitarz <ka.sitarz@gmail.com>
2019-11-23 16:44:10 +0100

    update icons for better browser compatibility

--- src/assets/manifest.js
+++ src/assets/manifest.js
@@ -7,13 +7,13 @@ module.exports = ({ name, endpoint }) => `
           "src": "/${endpoint}/icon-192.png",
           "type": "image/png",
           "sizes": "192x192",
-          "purpose": "maskable"
+          "purpose": "maskable any"
         },
         {
           "src": "/${endpoint}/icon-512.png",
           "type": "image/png",
           "sizes": "512x512",
-          "purpose": "maskable"
+          "purpose": "maskable any"
         }
       ],
     "start_url": "./",
