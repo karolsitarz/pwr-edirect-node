a134086a18caaf090ce07d09b4d57adcbf081dee
Karol Sitarz <ka.sitarz@gmail.com>
2020-01-17 11:45:11 +0100

    add cache-only service worker

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -4,26 +4,11 @@ self.addEventListener('install', function(evt) {
   evt.waitUntil(precache());
 });
 self.addEventListener('fetch', function(evt) {
-  evt.respondWith(
-    fromNetwork(evt.request, 400).catch(function() {
-      return fromCache(evt.request);
-    })
-  );
+  evt.respondWith(fromCache(evt.request));
 });
 function precache() {
   return caches.open(CACHE).then(function(cache) {
     return cache.addAll(['./', '../style.css']);
-  });
-}
-
-function fromNetwork(request, timeout) {
-  return new Promise(function(fulfill, reject) {
-    var timeoutId = setTimeout(reject, timeout);
-
-    fetch(request).then(function(response) {
-      clearTimeout(timeoutId);
-      fulfill(response);
-    }, reject);
   });
 }
 function fromCache(request) {
