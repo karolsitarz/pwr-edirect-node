af274d533ea6c191867c365ff7171cec6a3d909b
Karol Sitarz <ka.sitarz@gmail.com>
2020-02-18 20:22:41 +0100

    service worker fix

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -1,20 +1,31 @@
-var CACHE = 'network-or-cache';
+var CACHE = 'offline-cache';
 
-self.addEventListener('install', function(evt) {
-  evt.waitUntil(precache());
-});
-self.addEventListener('fetch', function(evt) {
-  evt.respondWith(fromCache(evt.request));
-});
-function precache() {
-  return caches.open(CACHE).then(function(cache) {
-    return cache.addAll(['./', '../style.css']);
-  });
-}
-function fromCache(request) {
-  return caches.open(CACHE).then(function(cache) {
-    return cache.match(request).then(function(matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
+self.addEventListener('install', e =>
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      cache.addAll([
+        './manifest.json',
+        './icon-512.png',
+        './icon-192.png'
+      ]);
+      return cache.addAll([
+        './',
+        '../style.css'
+      ]);
+    })
+  )
+);
+
+self.addEventListener('fetch', e =>
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(res => {
+        if (res) return res;
+        return fetch(e.request).then(res => {
+          cache.put(e.request, res.clone());
+          return res;
+        })
+      })
+    )
+  )
+);
